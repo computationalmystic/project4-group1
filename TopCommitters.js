66cbc21eebd231f221ca716b0c8a37316fa7df88
HenrySills <33098517+HenrySills@users.noreply.github.com>
2019-10-24 17:24:39 -0500

    Update TopCommitters.js

--- TopCommitters.js
+++ TopCommitters.js
@@ -22,3 +22,16 @@ function loadPullRequest() {
     xhttp.open("GET","http://augur.osshealth.io:5000/api/unstable/repo-groups/"+dropDown.options[dropDown.selectedIndex].value + "/pull-request-acceptance-rate", true);
     xhttp.send();
 }
+
+function loadClosedIssues() {
+    var dropDown = document.getElementById("dropDown");
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        var text = '{ "committers" : ' + xhttp.responseText + '}';
+        var obj = JSON.parse(text);
+        //add loop
+        document.getElementById("demo").innerHTML = obj.committers[0].email + "\t" + obj.committers[0].commits;
+    };
+    xhttp.open("GET","http://augur.osshealth.io:5000/api/unstable/repo-groups/"+dropDown.options[dropDown.selectedIndex].value + "/closed-issues-count", true);
+    xhttp.send();
+}
