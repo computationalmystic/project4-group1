1d6ab449e41bc6dec203f69d8ea75bbd828f40cf
HenrySills <33098517+HenrySills@users.noreply.github.com>
2019-10-27 12:59:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function clearPopulationArea(){
     document.getElementById("populationArea1").innerHTML = "";
     document.getElementById("populationArea2").innerHTML = "";
+    document.getElementById("populationArea3").innerHTML = "";
 }
 
 function repo_groupLoad(slide) {
@@ -20,6 +21,9 @@ function repo_groupLoad(slide) {
             } else if(slide == "populationArea2"){
                 document.getElementById("pageHeader").innerHTML = "Pull Requests";
                 stage = document.getElementById("populationArea2");
+            }else if(slide == "populationArea3"){
+                document.getElementById("pageHeader").innerHTML = "Closed Issues";
+                stage = document.getElementById("populationArea3");
             }
             stageHTML = "<ol>"
             for(i = 0; i < obj.repo_group.length; i++){
@@ -67,6 +71,8 @@ function loadVisuals(repoGroupID, repoID){
     
     console.log("This function was called");
     topCommitters(repoGroupID, repoID);
+    pullRequests(repoGroupID, repoID);
+    closedIssues(repoGroupID, repoID);
     
 }
 
@@ -101,6 +107,78 @@ function topCommitters(repoGroupID, reposID){
     };
     
     xhttp.open("GET","http://augur.osshealth.io:5000/api/unstable/repo-groups/" + repoGroupID + "/repos/" + reposID + "/top-committers", true);
+    xhttp.send();
+}
+
+function pullRequests(repoGroupID, reposID){
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = function() {
+        
+            if(this.readyState == 4 && this.status == 500){
+            console.log("There was an internal server error. Please try a different repository.");
+            error = document.getElementById("errorArea2");
+            error.innerHTML = "There was an internal server error. Please try a different repository."            
+        }
+        if(this.readyState == 4 && this.status == 200){
+            var text = '{ "pullRequests" : ' + xhttp.responseText + '}';
+            var obj = JSON.parse(text);
+            console.log(obj);
+            var email;
+            var i;
+            var committers = "<ol>"
+            for(i = 0;(obj.pullRequests[i].email != "other_contributors") && i < 10; i++){
+                console.log(i);
+                console.log(obj.pullRequests[i].email);
+            
+                email = obj.pullRequests[i].email;
+                committers = committers + "<li>" + email + "  " + obj.pullRequests[i].commits + "</li>";
+            }
+            committers = committers + "</ol>";
+            document.getElementById("populationArea2").innerHTML = committers;
+            document.getElementById("errorArea2").innerHTML = "";
+        }
+    };
+    
+    xhttp.open("GET","http://augur.osshealth.io:5000/api/unstable/repo-groups/" + repoGroupID + "/repos/" + reposID + "/pull-request-acceptance-rate", true);
+    xhttp.send();
+}
+
+ 
+
+
+
+function closedIssues(repoGroupID, reposID){
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = function() {
+        
+            if(this.readyState == 4 && this.status == 500){
+            console.log("There was an internal server error. Please try a different repository.");
+            error = document.getElementById("errorArea3");
+            error.innerHTML = "There was an internal server error. Please try a different repository."            
+        }
+        if(this.readyState == 4 && this.status == 200){
+            var text = '{ "closedIssues" : ' + xhttp.responseText + '}';
+            var obj = JSON.parse(text);
+            console.log(obj);
+            var email;
+            var i;
+            var committers = "<ol>"
+            for(i = 0;(obj.closedIssues[i].email != "other_contributors") && i < 10; i++){
+                console.log(i);
+                console.log(obj.closedIssues[i].email);
+            
+                email = obj.closedIssues[i].email;
+                committers = committers + "<li>" + email + "  " + obj.closedIssues[i].commits + "</li>";
+            }
+            committers = committers + "</ol>";
+            document.getElementById("populationArea3").innerHTML = committers;
+            document.getElementById("errorArea3").innerHTML = "";
+        }
+    };
+    
+    xhttp.open("GET","http://augur.osshealth.io:5000/api/unstable/repo-groups/" + repoGroupID + "/repos/" + reposID + "/closed-issues-count", true);
     xhttp.send();
 }
  
